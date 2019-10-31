9d33e59b6ff1beab103ed463ef2b92817aae9917
Harris Beg <harris@harrisbeg.com>
2019-10-30 20:54:04 -0700

    Obscure SECRET KEY
    Hey,
    
    Just a heads up - you might want to obscure your secret key from Django

--- omnibus/settings.py
+++ omnibus/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "j_h^-r9jfranxr*yt&i@mfra*3il7loub_xqhc29s^_arbr82h"
+SECRET_KEY = os.environ.get("SECRET_KEY","j_h^-r9jfranxr*yt&i@mfra*3il7loub_xqhc29s^_arbr82h")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
